ae3f9a04bef224643460698f370f9b1cd4c70d30
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2024-01-29 18:24:52 -0300

    JavaScript - Aulas 001 até 006

---
+++ modulo04-javascript/JS-I/aula006-tipos-de-dados/index.js
@@ -0,0 +1,17 @@
+// Tipos Primitivos de Dados
+console.log(typeof "JavaScript");
+console.log(typeof 10);
+console.log(typeof 12.16);
+console.log(typeof true);
+
+let variavel;
+console.log(typeof variavel);
+
+let variavelNula = null;
+console.log(typeof variavelNula);
+
+console.log(typeof console);
+console.log(typeof window);
+console.log(typeof document);
+
+console.log(typeof console.log);
