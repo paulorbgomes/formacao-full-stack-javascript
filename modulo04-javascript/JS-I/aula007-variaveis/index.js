ef9756990c68cea89a141531352a8816995fa716
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2024-01-29 22:17:40 -0300

    Aulas 007 até 010

---
+++ modulo04-javascript/JS-I/aula007-variaveis/index.js
@@ -0,0 +1,24 @@
+// Varáveis em JavaScript
+
+// Analisando o escopo da declaração var
+var var1 = "Paulo";
+console.log(var1);
+{
+  console.log(var1);
+}
+console.log(var1);
+
+// Analisando o escopo da declaração let
+/*
+{
+    let var2 = "Gomes";
+    console.log(var2);
+}
+console.log(var2);
+*/
+
+// Analisando o conceito de constante em JavaScript
+/*
+const var3 = 100;
+var3 = 200;
+*/
---
+++ modulo04-javascript/JS-I/aula008-operadores-expressoes/index.js
@@ -0,0 +1,40 @@
+// Operador de atribuição
+let var1 = 10;
+let var2 = 100;
+
+// Operadores aritméticos
+console.log(var1 + var2);
+console.log(var1 - var2);
+console.log(var1 * var2);
+console.log(var2 / var1);
+console.log(var2 % var1);
+
+// Operadores de comparação
+console.log("Operadores de Comparação");
+console.log(var1 != var2);
+console.log(var1 == var2);
+console.log(var1 === var2);
+console.log(var1 > var2);
+console.log(var1 >= var2);
+console.log(var1 < var2);
+console.log(var1 <= var2);
+
+// Operadores lógicos
+// OR (||)
+console.log("Operadores Lógicos - OR");
+console.log(false || false);
+console.log(false || true);
+console.log(true || false);
+console.log(true || true);
+
+// AND (&&)
+console.log("Operadores Lógicos - AND");
+console.log(false && false);
+console.log(false && true);
+console.log(true && false);
+console.log(true && true);
+
+// NOT (!)
+console.log("Operadores Lógicos - NOT");
+console.log(!true);
+console.log(!false);
