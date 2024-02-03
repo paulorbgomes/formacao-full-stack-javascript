01950f2f68a327dd2252ab8a257410274bfd3a26
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2024-02-03 15:31:10 -0300

    Aula 010

---
+++ modulo04-javascript/JS-III/aula010-envio-valores/index.js
@@ -0,0 +1,21 @@
+function register(element) {
+  /*
+  const nomeUsuario = element.children.username.value;
+  const password = element.children.password.value;
+  const passwordConfirmation = element.children.passwordConfirmation.value;
+
+  console.log(nomeUsuario);
+  console.log(password);
+  console.log(passwordConfirmation);
+  */
+
+  const nomeUsuario = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const passwordConfirmation = document.getElementById(
+    "passwordConfirmation"
+  ).value;
+
+  console.log(nomeUsuario);
+  console.log(password);
+  console.log(passwordConfirmation);
+}
