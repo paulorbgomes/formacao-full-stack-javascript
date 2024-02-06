38e1c798a02aed95654f7e663aaf14c8463ff091
Paulo R. B. Gomes <paulorbgomez@gmail.com>
2024-02-06 16:32:26 -0300

    Continuação módulo IV de JS

---
+++ modulo04-javascript/JS-IV/aula017-ES-modules/functions.js
@@ -0,0 +1,7 @@
+export function render() {
+  console.log("Renderizando a interface da aplicação ...");
+}
+
+export function store() {
+  console.log("Salvando as informações no banco de dados ...");
+}
---
+++ modulo04-javascript/JS-IV/aula017-ES-modules/index.js
@@ -0,0 +1,6 @@
+import { render, store } from "./functions.js";
+
+console.log("Aplicação iniciada ...");
+render();
+store();
+console.log("Aplicação finalizada ...");
